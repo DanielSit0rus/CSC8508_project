6ad692df1487c5f74ddf8cdd6f24dbf26c61808e
Kelvin Zhang <H.Zhang120@newcastle.ac.uk>
2025-01-30 16:21:02 +0000

    Import & test Fmod audio library
    1. Install FMod library. [in SDK\FMOD Studio API Windows & x64\Debug]
    2. Import the FMod library using CMake. [CMakeList.txt]
    3. Add some functions and variables (will be encapsulated into an audio system class later ) to test the FMod library. [main.cpp]
    4. Add some music and sound banks. [Assets\Sounds]

--- CSC8503/Main.cpp
+++ CSC8503/Main.cpp
@@ -33,6 +33,9 @@ using namespace CSC8503;
 #include <thread>
 #include <sstream>
 
+#include <fmod_studio.hpp>
+#pragma comment(lib, "fmodstudio_vc.lib")// link FMOD-Studio lib
+
 vector <Vector3> testNodes;
 
 void TestPathfinding() {
@@ -489,9 +492,49 @@ protected:
 	float pauseReminder = 1.0f;
 };
 
+//FMOD Audio Test (It will be encapsulated into an audio system class later)
+FMOD::Studio::System* studioSystem = nullptr;
+FMOD::Studio::Bank* masterBank = nullptr;
+FMOD::Studio::Bank* stringsBank = nullptr;
+FMOD::Studio::Bank* bgmBank = nullptr;
+FMOD::Studio::Bank* linBank = nullptr;
+FMOD::Studio::EventDescription* eventDescription = nullptr;
+FMOD::Studio::EventInstance* eventInstance = nullptr;
+void TestFmodStudioStart() {
+	FMOD::Studio::System::create(&studioSystem);
+	studioSystem->initialize(1024, FMOD_STUDIO_INIT_NORMAL, FMOD_INIT_NORMAL, nullptr);
 
+	studioSystem->loadBankFile("../Assets/Sounds/Banks/Master.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &masterBank);
+	studioSystem->loadBankFile("../Assets/Sounds/Banks/Master.strings.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &stringsBank);
+	studioSystem->loadBankFile("../Assets/Sounds/Banks/BGM.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &bgmBank);
+	studioSystem->loadBankFile("../Assets/Sounds/Banks/Lin.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &linBank);
+
+	studioSystem->getEvent("event:/BGM/BGM1", &eventDescription);
+	eventDescription->createInstance(&eventInstance);
+	eventInstance->start();
+}
+void TestFmodStudioUpdate() {
+	studioSystem->update();
+}
+void TestFmodStudioPause(bool isPaused) {
+	eventInstance->setPaused(isPaused);
+}
+void TestFmodStudioEnd() {
+	if (eventInstance)
+	{
+		eventInstance->stop(FMOD_STUDIO_STOP_ALLOWFADEOUT);
+		eventInstance->release();
+		eventInstance = nullptr;
+	}
+
+	masterBank->unload();
+	stringsBank->unload();
+	studioSystem->release();
+}
 
 int main() {
+	TestFmodStudioStart();
+
 	WindowInitialisation initInfo;
 	initInfo.width		= 1280;
 	initInfo.height		= 720;
@@ -556,6 +599,10 @@ int main() {
 		}
 		g->UpdateGame(dt);
 		gamescore = g->getscore();
+
+		TestFmodStudioUpdate();
+		TestFmodStudioPause(paused);
 	} 
 	Window::DestroyGameWindow();
+	TestFmodStudioEnd();
 }
