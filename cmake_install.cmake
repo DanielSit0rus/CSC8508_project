b2e7f91de453dfce8248f4f273a56f4beaaa5628
zhi-clz <c4067342@newcastle.ac.uk>
2025-02-03 22:32:58 +0800

    1.增加了Json库 2.在Main.cpp中增加了void JsonIOTest()用于测试

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -34,6 +34,11 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
+  include("D:/vs代码/team/CSC8508_project/SDK/nlohmannjson-json/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
   include("D:/vs代码/team/CSC8508_project/NCLCoreClasses/cmake_install.cmake")
 endif()
 
