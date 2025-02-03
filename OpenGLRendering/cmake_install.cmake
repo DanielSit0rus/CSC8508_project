766e54a5a611844fcb383da6860140c3ee173c83
tang <tang@LAPTOP-OH2SLUI9>
2025-02-03 11:24:13 +0000

    创建分支

--- OpenGLRendering/cmake_install.cmake
+++ OpenGLRendering/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/D_Project/cpp/CSC8508_project/OpenGLRendering
+# Install script for directory: D:/tang/Downloads/GitHub/CSC8508_project/OpenGLRendering
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
