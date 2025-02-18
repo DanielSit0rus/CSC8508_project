bd281037fc7c1a245677296ba937b981f0809efd
Daniel Hasian Lindung Sitorus <danielhls.dd@gmail.com>
2025-02-18 02:13:52 +0000

    multiple h files modified

--- OpenGLRendering/OGLMesh.h
+++ OpenGLRendering/OGLMesh.h
@@ -8,6 +8,7 @@ License: MIT (see LICENSE file at the top of the source tree)
 #pragma once
 #include "Mesh.h"
 #include "glad\gl.h"
+#include <memory>
 
 namespace NCL::Rendering {	
 	using UniqueOGLMesh		= std::unique_ptr<class OGLMesh>;
--- OpenGLRendering/OGLShader.h
+++ OpenGLRendering/OGLShader.h
@@ -8,6 +8,7 @@ License: MIT (see LICENSE file at the top of the source tree)
 #pragma once
 #include "Shader.h"
 #include "glad\gl.h"
+#include <memory>
 
 namespace NCL::Rendering {
 	using UniqueOGLShader = std::unique_ptr<class OGLShader>;
