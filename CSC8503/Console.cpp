7a2f7ffc1f70a36f53ca5dd583bf38b71d25e636
Kelvin Zhang <H.Zhang120@newcastle.ac.uk>
2025-02-20 14:38:12 +0000

    Console 1. finish basic console command 2. modify response of SLSystem & AudioSystem to Event

---
+++ CSC8503/Console.cpp
@@ -0,0 +1,56 @@
+#include "Console.h"
+
+using namespace NCL;
+using namespace CSC8503;
+
+void Console::Init() {
+    RegisterCommand("help", [this](const std::string&) { ShowHelp(); });
+    RegisterCommand("save", [this](const std::string&) { EventManager::Trigger(EventType::Data_Save); });
+    RegisterCommand("load", [this](const std::string&) { EventManager::Trigger(EventType::Data_Load); });
+}
+
+void Console::RegisterCommand(const std::string& command, CommandHandler handler) {
+    commands[command] = handler;
+}
+
+void Console::ProcessInput() {
+    while (true)
+    {
+        std::cout << ">";
+        std::getline(std::cin, input);
+        HandleCommand(input);
+        input.clear();
+    }
+}
+
+void Console::HandleCommand(const std::string& input) {
+    std::string command;
+    std::string args;
+
+    size_t spacePos = input.find(' ');
+    if (spacePos != std::string::npos) {
+        command = input.substr(0, spacePos);
+        args = input.substr(spacePos + 1);
+    }
+    else {
+        command = input;
+    }
+    auto it = commands.find(command);
+    if (it != commands.end()) {
+        it->second(args);
+    }
+    else {
+        std::cout << "Unknown command: " << command << std::endl;
+    }
+}
+
+#pragma region CommandFunc
+
+void Console::ShowHelp() const {
+    std::cout << "Available commands:" << std::endl;
+    for (const auto& cmd : commands) {
+        std::cout << " - " << cmd.first << std::endl;
+    }
+}
+
+#pragma endregion
---
+++ CSC8503/Console.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "EventManager.h"
+
+#include <iostream>
+#include <string>
+#include <functional>
+namespace NCL {
+    namespace CSC8503 {
+        class Console {
+        public:
+            using CommandHandler = std::function<void(const std::string&)>;
+
+            static Console& GetInstance() {
+                static Console instance;
+                return instance;
+            }
+            Console(const Console&) = delete;
+            Console& operator=(const Console&) = delete;
+
+            void Init();
+            void ProcessInput();
+            void RegisterCommand(const std::string& command, CommandHandler handler);
+
+        private:
+
+            Console() = default;
+            ~Console() = default;
+
+            void HandleCommand(const std::string& input);
+
+
+#pragma region CommandFunc
+            void ShowHelp() const;
+#pragma endregion
+
+
+
+            std::string input;
+            std::unordered_map<std::string, CommandHandler> commands;
+        };
+    }
+}
