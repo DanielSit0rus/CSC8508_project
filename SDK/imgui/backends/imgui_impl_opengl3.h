47772e45884da8c724ce4bd75176f9a94e93101d
Kelvin Zhang <H.Zhang120@newcastle.ac.uk>
2025-02-21 23:05:49 +0000

    import imgui

--- SDK/imgui/backends/imgui_impl_opengl3.h
+++ SDK/imgui/backends/imgui_impl_opengl3.h
@@ -26,7 +26,6 @@
 //  Only override if your GL version doesn't handle this GLSL version. See GLSL version table at the top of imgui_impl_opengl3.cpp.
 
 #pragma once
-#define _CRT_SECURE_NO_WARNINGS
 #include "imgui.h"      // IMGUI_IMPL_API
 #ifndef IMGUI_DISABLE
 
