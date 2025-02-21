608e668bf8de66169bcc717d82cab8d87bd542ca
lilhoo <c4012779@newcastle.ac.uk>
2025-02-21 10:47:23 +0000

    Preliminary design Statemachine ,needs improvement

---
+++ CSC8503/PaintballEnemy.h
@@ -0,0 +1,25 @@
+//#include "StateGameObject.h"
+//#include "PaintballGameObject.h"
+//#include "PaintballGameWorld.h"
+//
+//namespace NCL {
+//    namespace CSC8503 {
+//        class enemy : public StateGameObject {
+//        public:
+//            enemy();
+//            ~enemy();
+//
+//            virtual void Update(float dt);
+//            void SetPlayer(PaintballGameObject* playerObj);
+//            bool CanSeePlayer(); // 
+//
+//        private:
+//            void Patrol(float dt);
+//            void AttackPlayer(float dt); 
+//
+//            StateMachine* stateMachine;
+//            PaintballGameObject* player;
+//            //NavigationMesh* navMesh;
+//        };
+//    }
+//}
